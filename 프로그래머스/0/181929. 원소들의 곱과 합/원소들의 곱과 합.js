2a24f91e8cbb9287c6212ac85dd77faa31ab5b1e
Kim Hyo Jin <167041908+hyowls@users.noreply.github.com>
2024-04-17 11:58:48 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let add = 0
+    let mul = 1
+    for(let i=0;i<num_list.length; i++){
+        add += num_list[i]
+        mul *= num_list[i]
+    }
+    add = add * add
+    return mul < add ? 1 : 0
+}
