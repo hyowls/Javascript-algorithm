7d0476cc938cfb987f577e04f7f6f5c6cd4f7b55
Kim Hyo Jin <167041908+hyowls@users.noreply.github.com>
2024-04-30 10:28:55 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,8 @@
+function solution(x) {
+    let str = String(x).split("")
+    let sum = 0
+    for(let i=0; i<str.length;i++){
+        sum += Number(str[i])
+    }
+    return x % sum === 0 ? true : false
+}
