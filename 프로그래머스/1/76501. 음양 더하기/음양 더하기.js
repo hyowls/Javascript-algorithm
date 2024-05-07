c7a871c42f577d25079333c8b820b2ea20b40236
Kim Hyo Jin <167041908+hyowls@users.noreply.github.com>
2024-05-07 10:48:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,7 @@
+function solution(absolutes, signs) {
+    let result = 0;
+    for(let i=0; i < absolutes.length; i++){
+        (signs[i] === true)? result+= absolutes[i] : result+= absolutes[i]* -1
+    }
+    return result
+}
