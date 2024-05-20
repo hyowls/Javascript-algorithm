67dd3918b5b5a3b554f972f4ecd5b12bf62d6158
Kim Hyo Jin <167041908+hyowls@users.noreply.github.com>
2024-05-21 07:01:52 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,9 @@
+function solution(myString, pat) {
+    let result = 0;
+    for(let i=0; i < myString.length; i++){
+        if(myString.slice(i, i + pat.length) === pat){
+            result++
+        }
+    }
+    return result
+}
